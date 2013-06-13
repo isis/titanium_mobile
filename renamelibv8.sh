36ed9e55932a6e6ec30c9a782f8cd629252cb3c6
k-ishida <isis331@gmail.com>
2013-06-13 13:51:40 +0900

    add until for android module

---
+++ renamelibv8.sh
@@ -0,0 +1,16 @@
+mkdir ./dist/android/libv8/3.19.3/release/libs/mips
+mv  ./dist/android/libv8/3.19.3/release/libs/mipsel/libv8_base.mipsel.a             ./dist/android/libv8/3.19.3/release/libs/mips/libv8_base.a 
+mv  ./dist/android/libv8/3.19.3/release/libs/mipsel/libv8_nosnapshot.mipsel.a       ./dist/android/libv8/3.19.3/release/libs/mips/libv8_nosnapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/mipsel/libv8_snapshot.a                ./dist/android/libv8/3.19.3/release/libs/mips/libv8_snapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/x86/libv8_base.ia32.a                  ./dist/android/libv8/3.19.3/release/libs/x86/libv8_base.a
+mv  ./dist/android/libv8/3.19.3/release/libs/x86/libv8_nosnapshot.ia32.a            ./dist/android/libv8/3.19.3/release/libs/x86/libv8_nosnapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/x86/libv8_snapshot.a                   ./dist/android/libv8/3.19.3/release/libs/x86/libv8_snapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/armeabi/libv8_base.arm.a               ./dist/android/libv8/3.19.3/release/libs/armeabi/libv8_base.a
+mv  ./dist/android/libv8/3.19.3/release/libs/armeabi/libv8_nosnapshot.arm.a         ./dist/android/libv8/3.19.3/release/libs/armeabi/libv8_nosnapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/armeabi/libv8_snapshot.a               ./dist/android/libv8/3.19.3/release/libs/armeabi/libv8_snapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/armeabi-v7a/libv8_base.arm.a           ./dist/android/libv8/3.19.3/release/libs/armeabi-v7a/libv8_base.a
+mv  ./dist/android/libv8/3.19.3/release/libs/armeabi-v7a/libv8_nosnapshot.arm.a     ./dist/android/libv8/3.19.3/release/libs/armeabi-v7a/libv8_nosnapshot.a
+mv  ./dist/android/libv8/3.19.3/release/libs/armeabi-v7a/libv8_snapshot.a           ./dist/android/libv8/3.19.3/release/libs/armeabi-v7a/libv8_snapshot.a
+
+
+
